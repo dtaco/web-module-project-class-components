6ca83e5c2ba610b10b586bbec3cc8dbb8ff9658f
David Tacoronte <davidtacoronte@Davids-MacBook-Pro.local>
2023-08-07 11:13:56 -0400

    added state and basic tick functionality

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,39 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286,
+          completed: true
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        
+        },
+        
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
+
     return (
       <div>
         <h1>Todo List</h1>
         <ul>
-          <li>Walk the dog</li>
-          <li>Take out the trash</li>
-          <li>Workout</li>
+          {
+            todos.map(todo => {
+              return(
+                <li>{todo.name} { todo.completed?<span>- completed</span> : <span> </span> }</li>
+              )
+            })
+          }
         </ul>
 
         <form>
