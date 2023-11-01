493f16019c6aafee0f0d12d4112f630187ea23e4
David Tacoronte <dtaco@MBP-de-David.attlocal.net>
2023-11-01 15:31:02 -0400

    updated compelted status from COMPLETED to a text decoration of line-through and gray-out.

--- frontend/components/Todo.js
+++ frontend/components/Todo.js
@@ -6,7 +6,7 @@ export default class Todo extends React.Component {
   }
   render() {
     return(
-    <li onClick={this.handleClick}>{this.props.todo.name} {this.props.todo.completed?<span><strong>- COMPLETED</strong></span> : <span></span> }</li>
+    <li className={this.props.todo.completed? 'complete' : ''}onClick={this.handleClick}>{this.props.todo.name}</li>
     )
   }
 }
